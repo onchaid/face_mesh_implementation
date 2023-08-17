b0065e59807a06baca400b7315810f18b0b3be53
drgimenez <drgimenez@gmail.com>
2023-08-17 14:51:54 -0300

    Matrix validation with delta

--- src/validation/matrixValidation.js
+++ src/validation/matrixValidation.js
@@ -1,48 +1,42 @@
 function matrixValidation(identity, image) {
-  // Requirement: Both matrices must have the same dimensions
+    // Requirement: Both matrices must have the same dimensions
     const identityKeys = Object.keys(identity);
     const imageKeys = Object.keys(image);
+    if (identityKeys.length !== imageKeys.length) {
+        throw new Error('Matrix dimensions mismatch.');
+    }
+
     for (let i = 0; i < identityKeys.length; i++) {
-        if (identityKeys[i] !== imageKeys[i] || identity[identityKeys[i]].length !== image[imageKeys[i]].length) {
+        if (identity[identityKeys[i]].length !== image[imageKeys[i]].length) {
             throw new Error('Matrix dimensions mismatch.');
         }
     }
 
     const propertyNumber = identityKeys.length;
     let totalMetadata = 0;
-    let valueNumber;
-    let indexNumber;
     let matches = 0;
 
     // Matrix validation
-    // Loop through each property of identity
     for (let propertyScanId = 0; propertyScanId < propertyNumber; propertyScanId++) {
-        valueNumber = identity[identityKeys[propertyScanId]].length -1;
-        // Loop through each property of image
-        for (let propertyScanImg = 0; propertyScanImg < propertyNumber; propertyScanImg++) {
-            // Value check cell by cell
-            for (let valueScan = 0; valueScan < valueNumber; valueScan++) {
-                indexNumber = Object.keys(identity[identityKeys[propertyScanId]][valueScan]).length -1;
-                //indexNumber = Object.keys(image[imageKeys[propertyScanImg]][valueScan]).length;
-                // Value heck x, y , z
-                for (let indexScan = 0; indexScan < indexNumber; indexScan++) {
-                    totalMetadata++;
-                    console.log(valueNumber);
-                    console.log(image["rightEye"][valueScan][Object.keys(image[imageKeys[propertyScanImg]][0])[indexScan]]);
-                    console.log();
-                    //if (identity[identityKeys[propertyScanId]][valueScan][Object.keys(identity[identityKeys[propertyScanId]][0])[indexScan]] 
-                    //    ===
-                    //    image[imageKeys[propertyScanImg]][valueScan][Object.keys(image[imageKeys[propertyScanImg]][0])[indexScan]]) {
-                    //    matches++;
-                   // }
+        const valueNumber = identity[identityKeys[propertyScanId]].length;
+        for (let valueScan = 0; valueScan < valueNumber; valueScan++) {
+            const indexNumber = Object.keys(identity[identityKeys[propertyScanId]][valueScan]).length;
+            for (let indexScan = 0; indexScan < indexNumber; indexScan++) {
+                const identityValue = identity[identityKeys[propertyScanId]][valueScan][Object.keys(identity[identityKeys[propertyScanId]][valueScan])[indexScan]];
+                const imageValue = image[imageKeys[propertyScanId]][valueScan][Object.keys(image[imageKeys[propertyScanId]][valueScan])[indexScan]];
+                
+                totalMetadata++;
+                const delta = Math.floor(imageValue) * 0.18;
+                if (Math.floor(identityValue) >= Math.floor(imageValue) - Math.floor(delta)
+                &&
+                Math.floor(identityValue) <= Math.floor(imageValue) + Math.floor(delta)) {
+                    matches++;
                 }
             }
         }
     }
 
     // Match level
-    console.log(propertyScanImg);
-    console.log(valueScan);
     const matchLevel = (matches / totalMetadata) * 100;
 
     return matchLevel;
